9a774eb1da17f082928d69d4ab67d93528529417
vikramBhavsar <vikram.bhavsar@somaiya.edu>
2021-12-26 12:56:35 +0530

    Added working Carousel

--- src/app/research/research.component.ts
+++ src/app/research/research.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, Renderer2 } from '@angular/core';
 import { faChevronLeft,faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -8,12 +8,68 @@ import { faChevronLeft,faChevronRight } from '@fortawesome/free-solid-svg-icons'
 })
 export class ResearchComponent implements OnInit {
 
+  // icon variables
   faChevronLeft = faChevronLeft;
   faChevronRight = faChevronRight;
 
-  constructor() { }
+
+  // getting elements
+  @ViewChild('ResearchCarousel') researchCarousel!: ElementRef;
+
+
+
+
+  // presentation variables
+  curResearch!:number;
+  totalResearch!:number;
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+
+    // setting values for all the variables
+    this.curResearch = 0;
+    
   }
+  
+  
+  ngAfterViewInit(){
+    // set initial width for class 'research-carousel__research__OC__container'
+    this.renderer.setStyle(this.researchCarousel.nativeElement,"width","360rem");
+
+    //setting the number of child research has
+    this.totalResearch = this.researchCarousel.nativeElement.childElementCount;
+  }
+
+
+
+  // methods called from html
+  nextResearch(){
+    this.curResearch += 1;
+
+    if(this.curResearch > (this.totalResearch - 1)){
+      this.curResearch = 0;
+    }
+
+    // moving the carousel
+    this.renderer.setStyle(this.researchCarousel.nativeElement,"transform",`translateX(-${this.curResearch * 60}rem)`);
+
+  }
+
+
+  prevResearch(){
+    this.curResearch -= 1;
+
+    if(this.curResearch < 0){
+      this.curResearch = this.totalResearch - 1;
+    }
+
+    // moving the carousel
+    this.renderer.setStyle(this.researchCarousel.nativeElement,"transform",`translateX(-${this.curResearch * 60}rem)`);
+
+  }
+
+
+
 
 }
